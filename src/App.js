ed3c052045216c62ebafe3dabf935d9cc69d6fac
Mridul <gmridul556@gmail.com>
2022-06-07 04:30:12 +0530

    Routers for pages created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
 
 function App() {
-  return (
-    <div className="App">
-    <h1> Hello </h1>
-    </div>
-  );
+    return (
+        <>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route
+                        path="/editor/:roomId"
+                        element={<EditorPage />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
